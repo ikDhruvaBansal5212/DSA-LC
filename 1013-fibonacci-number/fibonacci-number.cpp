be740aeb8089330df77fbd727527d2cd5363db97
Dhruv Bansal <dbansal1_be23@thapar.edu>
2026-05-29 16:51:33 +0530

    Time: 14 ms (21.59%) | Memory: 7.6 MB (88.09%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        return solve(n);
+    }
+
+    int solve(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        return solve(n-1)+solve(n-2);
+    }
+};
