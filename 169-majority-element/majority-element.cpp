322e9c366dead48ea8ab87d087d2d59861953958
Dhruv Bansal <dbansal1_be23@thapar.edu>
2026-06-01 12:07:56 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (86.99%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt=0;
+        int ele;
+        for(int i=0;i<nums.size();i++){
+            if(cnt==0){
+                cnt++;
+                ele=nums[i];
+            }
+            else if(ele==nums[i]){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+        return ele;
+    }
+};
