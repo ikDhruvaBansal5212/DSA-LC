159ea55bcb71ce9ade26f26a6ce513d29a89d710
Dhruv Bansal <dbansal1_be23@thapar.edu>
2026-06-19 13:20:24 +0530

    Time: 2 ms (72.85%) | Memory: 10.3 MB (59.20%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<char>> board(n, vector<char>(n, '.'));
+        vector<string> board(n, string(n, '.'));
         vector<vector<string>> res;
 
-       combinations(board,0,0,n,res);
+        vector<int> leftrow(n,0), upperdiag(2*n-1,0), lowerdiag(2*n-1,0);
+
+       solve(board,0,0,n,res,leftrow,upperdiag,lowerdiag);
        return res;
 
     }
 
-    void combinations(vector<vector<char>> &board,int row,int p , int n , vector<vector<string>> &res){
+    void solve(vector<string> &board,int row,int p , int n , vector<vector<string>> &res,vector<int>&leftrow,vector<int> &upperdiag,vector<int> &lowerdiag){
         if(p==n){
-            vector<string> temp;
-            for (int i = 0; i < n; i++) {
-                temp.push_back(string(board[i].begin(), board[i].end()));
-            }
-            res.push_back(temp);
+            res.push_back(board);
             return;
         }
        
         for(int j=0;j<n;j++){
-            if(board[row][j]=='.' && isvalid(row,j,n,board)){
-                    board[row][j]='Q';
-                    combinations(board,row+1,p+1,n,res);
-                    board[row][j]='.';
-                }
+            if(leftrow[j]==0 && lowerdiag[row+j]==0 && upperdiag[n-1+j-row]==0){
+
+                board[row][j]='Q';
+                leftrow[j]=1 ;
+                lowerdiag[row+j]=1;
+                upperdiag[n-1+j-row]=1;
+
+                solve(board,row+1,p+1,n,res,leftrow,upperdiag,lowerdiag);
+
+                board[row][j]='.';
+                leftrow[j]=0 ;
+                lowerdiag[row+j]=0;
+                upperdiag[n-1+j-row]=0;
+
             }
+        
     
     }
 
-     bool isvalid(int i, int j, int n, vector<vector<char>> &board) {
+     /* bool isvalid(int i, int j, int n, vector<vector<char>> &board) {
         // Check column
         for (int k = 0; k < i; k++) {
             if (board[k][j] == 'Q') return false;
         }
 
-        // Diagonal (top-left)
+        // Diagonal (top-left)  
         for (int r = i - 1, c = j - 1; r >= 0 && c >= 0; r--, c--) {
             if (board[r][c] == 'Q') return false;
         }
@@ -46,5 +54,6 @@ public:
         }
 
         return true;
+    } */
     }
 };
