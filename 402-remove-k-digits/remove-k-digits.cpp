b25846ab17c060cc1ff6d4b08d77374b3f7c10f1
Dhruv Bansal <dbansal1_be23@thapar.edu>
2026-06-26 12:49:18 +0530

    Time: 5 ms (34.10%) | Memory: 11.8 MB (22.19%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+
+        stack<char>st;
+        for(char c : num){
+
+            while(!st.empty() && k>0 && st.top()>c){
+                st.pop();
+                k--;
+            }
+
+            st.push(c);
+        }
+
+        while(k>0 && !st.empty()){
+            st.pop();
+            k--;
+        }
+
+        string res;
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+
+        while(res.size()>0 && res.back()=='0'){
+            res.pop_back();
+        }
+
+        reverse(res.begin(),res.end());
+        return res.empty() ? "0" : res;
+    }
+};
