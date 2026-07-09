61b12ec300c666008dcd31c8fb18a2f6c8847357
Dhruv Bansal <dbansal1_be23@thapar.edu>
2026-07-09 15:11:20 +0530

    Time: 22 ms (89.08%) | Memory: 17.6 MB (43.87%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    void solve(int i,int j,vector<vector<char>>& grid,vector<vector<int>>& vis,int m,int n){
+        vis[i][j]=1;
+
+        int col[]={0,1,0,-1};
+        int row[]={1,0,-1,0};
+
+        for(int p=0;p<4;p++){
+            int nrow=i+row[p];
+            int ncol=j+col[p];
+
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]=='1'){
+                solve(nrow,ncol,grid,vis,m,n);
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        int res=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && vis[i][j]!=1){
+                    res++;
+                    solve(i,j,grid,vis,m,n);
+                }
+            }
+        }
+        return res;
+    }
+};
